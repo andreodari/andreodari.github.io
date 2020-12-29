fcdbd8462e8d0da293b2046eb90ae14e2e211478
andreodari <softmasters.a@outlook.com>
2020-12-29 09:37:12 +0200

    1

--- TkgyCZ7xsFqHiERJ/old/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".trigger").click(function(){
-        numIndex = $(this).index();
-        $(".active-card").removeClass("active-card");
-        $(".card:eq(" + numIndex + ")").addClass("active-card");
-        $(".trigger").removeClass("active-trigger");
-        $(".trigger:eq(" + numIndex + ")").addClass("active-trigger");
-    });
-});
